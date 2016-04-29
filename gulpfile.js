cb04905f2e44dd5eaed6070568cbbdd2ad126c17
Chris He <chrishe@office.citic1616.com>
2016-04-29 16:50:15 +0800

    Add components

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var gulp 	= require('gulp');
+var sass 	= require('gulp-sass');
+var wiredep = require('wiredep').stream;
+/* 
+gulp.task('bower', function () {  
+  wiredep({
+    src: './index.html',
+    directory: './bower_componets/',
+    bowerJson: require('./bower.json'),
+  });
+});
+
+*/
+gulp.task('bower', function () {  
+  gulp.src('./index.html')
+    .pipe(wiredep({
+      optional: 'configuration',
+      goes: 'here'
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+/** gulp-sass **/
+gulp.task('sass', function () {
+	return gulp.src('./scss/*.scss')
+		.pipe(sass()
+			  .on('error', sass.logError)
+			 )
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+	gulp.watch('./scss/*.scss', ['sass']);
+});
+
+
+/** tasks **/
+gulp.task('default', ['bower','sass:watch']);
