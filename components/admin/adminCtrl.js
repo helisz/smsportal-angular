5c3ce7d828e4a4798301f68a95190793af88e81d
Chris He <chrishe@office.citic1616.com>
2016-05-06 10:29:48 +0800

    O

---
+++ components/admin/adminCtrl.js
@@ -0,0 +1,9 @@
+admin.controller('adminCtrl', function ($scope) {
+	//$scope.helloText = "E";
+	//$scope.descriptionText = "D";
+	
+    this.userName = 'Example user';
+    this.helloText = 'Welcome in SeedProject';
+    this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
+
+});
